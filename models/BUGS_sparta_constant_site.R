2b9dacdd6de3dd777f692e3c588fb3d12ccfa7be
Diana Bowler <diana.bowler@nina.no>
2018-06-18 09:38:00 +0200

    Run analysis at site level, rather than year/site

---
+++ models/BUGS_sparta_constant_site.R
@@ -0,0 +1,46 @@
+setwd("C:/Users/diana.bowler/OneDrive - NINA/Alpine/ptarmiganUpscaling/models")
+
+cat("
+  model{
+  # JAGS code for SPARTA model 
+  # on the year effect of the state model + intercept 
+  
+  # State model
+  for (i in 1:nsite){ 
+      z[i] ~ dbern(muZ[i]) 
+      logit(muZ[i]) <- int.alpha
+    }
+  
+  ### Observation Model
+  for(j in 1:nvisit) {
+
+    y[j] ~ dbern(Py[j]) #data is Y
+    
+    Py[j]<- z[site[j]]*p[j] #probability to detect = prob of occ * prob of detection
+
+    logit(p[j]) <-  int.d + a[year[j]] 
+
+  } 
+
+  #Priors 
+    mean.psi ~ dunif(0, 1)       # Occupancy intercept on prob. scale
+    int.alpha <- logit(mean.psi)
+    mean.p ~ dunif(0, 1)         # Detection intercept on prob. scale
+    int.d <- logit(mean.p)
+
+    #random year effects
+    for(t in 1:nyear){
+      a[t] ~ dnorm(0, tau.y)
+    }
+    tau.y <- 1/(sd.y * sd.y)
+    sd.y ~ dunif(0,10) 
+    
+    #site effects
+    for (i in 1:nsite) {
+      eta[i] ~ dnorm(0, tau.s)       
+    } 
+    tau.s <- 1/(sd.s * sd.s) 
+    sd.s ~ dunif(0,10)
+
+  }
+    ",fill=TRUE,file="BUGS_sparta_constant_site.txt")
