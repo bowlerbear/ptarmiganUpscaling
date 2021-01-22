bcf4381fd29d2985df8b6ab429922d5cf51466af
Diana Bowler <diana.bowler@idiv.de>
2021-01-22 17:59:56 +0100

    updating simple lm results

--- linetransects_combinedModel_Buffers.R
+++ linetransects_combinedModel_Buffers.R
@@ -74,6 +74,8 @@ bufferData <- readRDS("data/varDF_allEnvironData_buffers_idiv.rds")
 siteMeans <- merge(siteMeans,bufferData,by="LinjeID")
 siteMeans <- subset(siteMeans,!is.na(tree_line))
 
+siteMeans$adm <- mergeCounties(siteMeans$adm,further=TRUE)#needs to be done again
+
 ### get spatial polygons ###########################################
 
 Polys_spatial <- readRDS("data/Polys_spatial.rds")
@@ -100,24 +102,24 @@ tm_shape(NorwayOrig) +
 hist(siteMeans$meanNu)
 summary(siteMeans$meanNu)
 
-glm1 <- glm(log(meanNu+1) ~ scale(bio1) + scale(bio5) + scale(bio6) + 
+glm1 <- lm(log(meanNu+1) ~ scale(bio1) + scale(bio5) + scale(bio6) + 
             Open + PrefOpen + PrefClosed +
             scale(tree_line) + 
             scale(elevation)+
             scale(elevation^2),
             data=siteMeans)
 summary(glm1)
-  #                           Estimate Std. Error t value Pr(>|t|)    
-  # (Intercept)                1.9298457  0.1438699  13.414  < 2e-16 ***
-  # scale(bio1)                0.1892296  0.0614568   3.079 0.002171 ** 
-  # scale(bio5)               -0.0766258  0.0363032  -2.111 0.035207 *  
-  # scale(bio6)                0.0357897  0.0567560   0.631 0.528548    
-  # Open                       0.0017925  0.0008570   2.092 0.036883 *  
-  # PrefOpen                   0.0006795  0.0006042   1.125 0.261183    
-  # PrefClosed                -0.0012863  0.0016000  -0.804 0.421760    
-  # scale(tree_line_position) -0.1207165  0.0357055  -3.381 0.000769 ***
-  # scale(elevation)           0.7300731  0.1163483   6.275 6.68e-10 ***
-  # scale(elevation^2)        -0.7514253  0.1225348  -6.132 1.57e-09 ***
+#                       Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)         1.9044860  0.1466637  12.985  < 2e-16 ***
+#   scale(bio1)         0.1607169  0.0633338   2.538  0.01142 *  
+#   scale(bio5)        -0.0525747  0.0364160  -1.444  0.14935    
+#   scale(bio6)         0.0653411  0.0593270   1.101  0.27119    
+#   Open                0.0014211  0.0007037   2.019  0.04389 *  
+#   PrefOpen            0.0006222  0.0005126   1.214  0.22537    
+#   PrefClosed         -0.0006118  0.0013092  -0.467  0.64044    
+#   scale(tree_line)   -0.2987196  0.0979224  -3.051  0.00239 ** 
+#   scale(elevation)    1.0294777  0.1732242   5.943 4.79e-09 ***
+#   scale(elevation^2) -0.6955471  0.1222922  -5.688 2.03e-08 ***
   
 library(MuMIn)
 options(na.action = "na.fail")
@@ -128,24 +130,25 @@ subset(dd, delta < 2)
 #corrected by transect length
 summary(siteMeans$meanNu/siteMeans$meanTL)
 hist(siteMeans$meanNu/siteMeans$meanTL)
-glm1 <- lm(meanNu/meanTL ~ scale(bio1) + scale(bio5) + scale(bio6) + 
+glm1 <- lm(log(meanNu/meanTL+1) ~ scale(bio1) + scale(bio5) + scale(bio6) + 
               Open + PrefOpen + PrefClosed +
-              scale(tree_line_position) + 
+              scale(tree_line) + 
               scale(elevation)+scale(elevation^2),
               data=siteMeans)
 summary(glm1)
 Coefficients:
-  #                             Estimate Std. Error t value Pr(>|t|)    
-  # (Intercept)                3.816e-03  5.429e-04   7.028 5.68e-12 ***
-  # scale(bio1)                3.771e-04  2.319e-04   1.626  0.10451    
-  # scale(bio5)               -6.277e-04  1.370e-04  -4.582 5.61e-06 ***
-  # scale(bio6)                4.577e-04  2.142e-04   2.137  0.03301 *  
-  # Open                      -3.156e-06  3.234e-06  -0.976  0.32953    
-  # PrefOpen                   1.573e-06  2.280e-06   0.690  0.49043    
-  # PrefClosed                -1.565e-05  6.038e-06  -2.592  0.00976 ** 
-  # scale(tree_line_position) -2.049e-04  1.347e-04  -1.521  0.12881    
-  # scale(elevation)           2.808e-03  4.391e-04   6.394 3.23e-10 ***
-  # scale(elevation^2)        -2.440e-03  4.624e-04  -5.276 1.84e-07 ***
+  Coefficients:
+  #                       Estimate Std. Error t value Pr(>|t|)    
+  # (Intercept)         3.664e-03  5.397e-04   6.788 2.78e-11 ***
+  # scale(bio1)         3.241e-04  2.331e-04   1.390   0.1649    
+  # scale(bio5)        -5.421e-04  1.340e-04  -4.045 5.93e-05 ***
+  # scale(bio6)         5.047e-04  2.183e-04   2.312   0.0211 *  
+  # Open               -2.339e-06  2.590e-06  -0.903   0.3669    
+  # PrefOpen            1.398e-06  1.887e-06   0.741   0.4590    
+  # PrefClosed         -9.982e-06  4.818e-06  -2.072   0.0387 *  
+  # scale(tree_line)   -5.929e-04  3.604e-04  -1.645   0.1005    
+  # scale(elevation)    3.321e-03  6.375e-04   5.209 2.63e-07 ***
+  # scale(elevation^2) -2.236e-03  4.500e-04  -4.969 8.82e-07 ***
 
 ### brt #########################################################################
 
@@ -161,24 +164,24 @@ brt1 <- gbm.step(data=siteMeans,
                  family = 'gaussian')
 
 summary(brt1)
-#                                 var    rel.inf
-# tree_line                   tree_line 25.0060644
-# bio6                             bio6 20.8827893
-# bio5                             bio5 12.3456494
-# elevation                   elevation  9.8402625
-# PrefOpen                     PrefOpen  9.1553879
-# tree_line_position tree_line_position  4.2568394
-# bio1                             bio1  4.0936455
-# PrefClosed                 PrefClosed  3.1023779
-# Open                             Open  3.0298239
-# Forest                         Forest  2.3967398
-# Top                               Top  1.9409646
-# alpine_habitat3       alpine_habitat3  1.5355663
-# alpine_habitat1       alpine_habitat1  0.8527290
-# Bottom                         Bottom  0.6128414
-# alpine_habitat2       alpine_habitat2  0.5938059
-# Agriculture               Agriculture  0.3545129
-# alpine_habitat4       alpine_habitat4  0.0000000
+#                                   var     rel.inf
+# bio6                             bio6 23.59032517
+# tree_line                   tree_line 22.84091205
+# bio5                             bio5 12.76908910
+# elevation                   elevation 10.83059745
+# PrefOpen                     PrefOpen  7.75299072
+# bio1                             bio1  3.76294838
+# alpine_habitat2       alpine_habitat2  3.16039800
+# tree_line_position tree_line_position  3.05851428
+# Top                               Top  2.79262098
+# Open                             Open  2.37575864
+# alpine_habitat3       alpine_habitat3  2.08569589
+# PrefClosed                 PrefClosed  1.86873326
+# Bottom                         Bottom  1.45702810
+# Forest                         Forest  1.41170978
+# Agriculture               Agriculture  0.20984804
+# alpine_habitat1       alpine_habitat1  0.03283015
+# alpine_habitat4       alpine_habitat4  0.00000000
 
 #plot main effects
 gbm.plot(brt1, n.plots=8, write.title = TRUE)
