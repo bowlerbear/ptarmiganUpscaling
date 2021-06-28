fdfa614c18f4668a3b1e6398b17b534a1c8d9ee1
Diana Bowler <diana.bowler@idiv.de>
2021-06-28 14:22:26 +0200

    scale and define covariates

--- HPC_linetransects_combinedModel_Buffers.R
+++ HPC_linetransects_combinedModel_Buffers.R
@@ -46,7 +46,8 @@ sum(tlDF$length==0)#1302
 
 ### get environ data #################################################
 
-bufferData <- readRDS(paste(myfolder,"varDF_allEnvironData_buffers_idiv.rds",sep="/"))
+bufferData <- readRDS(paste(myfolder,
+                            "varDF_allEnvironData_buffers_idiv.rds",sep="/"))
 #why are some missing???
 tlDF <- subset(tlDF, LinjeID %in% bufferData$LinjeID)
 
@@ -78,6 +79,15 @@ sum(as.numeric(totalsInfo),na.rm=T)
 #check alignment with other datasets
 all(row.names(groupInfo)==siteInfo$siteIndex)
 
+### site abundances ##############################################
+
+# siteSummary <- tlDF %>%
+#                 filter(!is.na(totalsInfo)) %>%
+#                 group_by(siteIndex) %>%
+#                 summarise(nuZeros = sum(totalsInfo==0),meanCount = mean(totalsInfo))
+# table(siteSummary$nuZeros)  
+# summary(siteSummary$meanCount) #all above zero 
+
 ### detection data ################################################
 
 allDetections <- subset(allData, !is.na(totalIndiv) & totalIndiv!=0 
@@ -87,7 +97,6 @@ allDetections$siteIndex <- siteInfo$siteIndex[match(allDetections$LinjeID,
                                                     siteInfo$LinjeID)]
 allDetections$admN <- siteInfo$admN[match(allDetections$LinjeID,
                                                     siteInfo$LinjeID)]
-
 
 siteInfo_ArtsDaten$admNgrouped <- siteInfo$admN[match(siteInfo_ArtsDaten$admGrouped,
                                                       siteInfo$adm)]
@@ -121,119 +130,222 @@ bugs.data$GroupSizes[is.na(bugs.data$GroupSizes)] <- 0
 
 ### get environ data #########################################
 
-#make the number smaller
-
-siteInfo_ArtsDaten$distCoast <- siteInfo_ArtsDaten$distCoast/10000
-siteInfo_ArtsDaten$elevation <- siteInfo_ArtsDaten$elevation/100  
-siteInfo_ArtsDaten$bio1 <- siteInfo_ArtsDaten$bio1/10
-siteInfo_ArtsDaten$bio5 <- siteInfo_ArtsDaten$bio5/100  
-siteInfo_ArtsDaten$bio6 <- siteInfo_ArtsDaten$bio6/100    
-siteInfo_ArtsDaten$tree_line <- siteInfo_ArtsDaten$tree_line/100
-siteInfo_ArtsDaten$y <- siteInfo_ArtsDaten$y/1000000
-
-
-bufferData$distCoast <- bufferData$distCoast/10000
-bufferData$elevation <- bufferData$elevation/100  
-bufferData$bio1 <- bufferData$bio1/10
-bufferData$bio5 <- bufferData$bio5/100  
-bufferData$bio6 <- bufferData$bio6/100    
-bufferData$tree_line <- bufferData$tree_line/100
-bufferData$y <- bufferData$y/1000000
-
-#add new variables to the bugs data
 all(bufferData$LinjeID==siteInfo$LinjeID)
-bugs.data$occDM <- model.matrix(~ bufferData$y +
-                                  bufferData$y^2 +#insig
-                                  bufferData$bio6 +
-                                  bufferData$distCoast +
-                                  bufferData$bio5 +
-                                  bufferData$tree_line +
-                                  bufferData$elevation +#insig
-                                  bufferData$Meadows +#insig
-                                  bufferData$Bog +#insig
-                                  bufferData$Forest)[,-1]
-bugs.data$n.covs <- ncol(bugs.data$occDM)
 
+myVars <- c("bio1", "bio5", "bio6","MountainBirchForest", "Bog","ODF",
+            "Meadows","OSF","Mire","SnowBeds",
+            "tree_line_position","tree_line",
+            "y","distCoast")
+
+# scale them
+bufferData <- bufferData[,c("LinjeID",myVars)]
+bufferMeans <- as.numeric(apply(bufferData,2,mean))
+bufferSD <- as.numeric(apply(bufferData,2,sd))
+for(i in 2:ncol(bufferData)){
+  bufferData[,i] <- (bufferData[,i] - bufferMeans[i])/bufferSD[i]
+}
+
+#also for the siteInfo_ArtsDaten with same scaling
+siteInfo_ArtsDaten <- siteInfo_ArtsDaten[,c("grid",myVars)]
+for(i in 2:ncol(siteInfo_ArtsDaten)){
+  siteInfo_ArtsDaten[,i] <- (siteInfo_ArtsDaten[,i] - bufferMeans[i])/bufferSD[i]
+}
+
+### choose model ##############################################
+
+modelTaskID <- read.delim(paste(myfolder,"modelTaskID_distanceModel.txt",sep="/"),as.is=T)
+
+#get task id
+task.id = as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID", "1"))
+
+#get model for this task
+mymodel <- modelTaskID$Model[which(modelTaskID$TaskID==task.id)]
+
+### standard model  ###########################################
+
+#variables selected based on first simple analyses
+
+if(mymodel == "linetransectModel_variables.txt"){
+  
+#add new variables to the bugs data
+bugs.data$occDM <- model.matrix(~ bufferData$y +
+                                  bufferData$bio6 +
+                                  I(bufferData$bio6^2) +
+                                  bufferData$distCoast +
+                                  I(bufferData$distCoast^2) +
+                                  bufferData$bio5 +
+                                  I(bufferData$bio5^2) +
+                                  bufferData$tree_line +
+                                  I(bufferData$tree_line^2) +
+                                  bufferData$OSF +
+                                  bufferData$SnowBeds)[,-1]
 
 #predictions to full grid
 bugs.data$predDM <- model.matrix(~ siteInfo_ArtsDaten$y +
-                                  siteInfo_ArtsDaten$y^2 +#insig
-                                  siteInfo_ArtsDaten$bio6 +
-                                  siteInfo_ArtsDaten$distCoast +
+                                   siteInfo_ArtsDaten$bio6 +
+                                   I(siteInfo_ArtsDaten$bio6^2) +
+                                   siteInfo_ArtsDaten$distCoast +
+                                   I(siteInfo_ArtsDaten$distCoast^2) +
+                                   siteInfo_ArtsDaten$bio5 +
+                                   I(siteInfo_ArtsDaten$bio5^2) +
+                                   siteInfo_ArtsDaten$tree_line +
+                                   I(siteInfo_ArtsDaten$tree_line^2) +
+                                   siteInfo_ArtsDaten$OSF +
+                                   siteInfo_ArtsDaten$SnowBeds)[,-1]
+
+### indicator model selection ################################
+
+#all linear and select quadratic
+} else if (mymodel == "linetransectModel_variables_ModelSelection.txt"){ 
+  
+bugs.data$occDM <- model.matrix(~ bufferData$bio6 +
+                                  bufferData$bio5 +
+                                  bufferData$y +
+                                  bufferData$distCoast +
+                                  bufferData$tree_line +
+                                  bufferData$MountainBirchForest +
+                                  bufferData$Bog +
+                                  bufferData$ODF +
+                                  bufferData$Meadows +
+                                  bufferData$OSF +
+                                  bufferData$Mire +
+                                  bufferData$SnowBeds +
+                                  I(bufferData$bio6^2) +
+                                  I(bufferData$bio5^2) +
+                                  I(bufferData$y^2) +
+                                  I(bufferData$distCoast^2) +
+                                  I(bufferData$tree_line^2) +
+                                  I(bufferData$MountainBirchForest^2) +
+                                  I(bufferData$Bog^2) +
+                                  I(bufferData$ODF^2) +
+                                  I(bufferData$Meadows^2) +
+                                  I(bufferData$OSF^2) +
+                                  I(bufferData$Mire^2) +
+                                  I(bufferData$SnowBeds^2))[,-1]
+
+# #predictions to full grid
+bugs.data$predDM <- model.matrix(~ siteInfo_ArtsDaten$bio6 +
                                   siteInfo_ArtsDaten$bio5 +
+                                  siteInfo_ArtsDaten$y +
+                                  siteInfo_ArtsDaten$distCoast +
                                   siteInfo_ArtsDaten$tree_line +
-                                  siteInfo_ArtsDaten$elevation +#insig
-                                  siteInfo_ArtsDaten$Meadows +#insig
-                                  siteInfo_ArtsDaten$Bog +#insig
-                                  siteInfo_ArtsDaten$Forest)[,-1]
+                                  siteInfo_ArtsDaten$MountainBirchForest +
+                                  siteInfo_ArtsDaten$Bog +
+                                  siteInfo_ArtsDaten$ODF +
+                                  siteInfo_ArtsDaten$Meadows +
+                                  siteInfo_ArtsDaten$OSF +
+                                  siteInfo_ArtsDaten$Mire +
+                                  siteInfo_ArtsDaten$SnowBeds +
+                                  I(siteInfo_ArtsDaten$bio6^2) +
+                                  I(siteInfo_ArtsDaten$bio5^2) +
+                                  I(siteInfo_ArtsDaten$y^2) +
+                                  I(siteInfo_ArtsDaten$distCoast^2) +
+                                  I(siteInfo_ArtsDaten$tree_line^2) +
+                                  I(siteInfo_ArtsDaten$MountainBirchForest^2) +
+                                  I(siteInfo_ArtsDaten$Bog^2) +
+                                  I(siteInfo_ArtsDaten$ODF^2) +
+                                  I(siteInfo_ArtsDaten$Meadows^2) +
+                                  I(siteInfo_ArtsDaten$OSF^2) +
+                                  I(siteInfo_ArtsDaten$Mire^2) +
+                                  I(siteInfo_ArtsDaten$SnowBeds^2))[,-1]
+
+### lasso model ##############################################
+
+} else if (mymodel == "linetransectModel_variables_LASSO.txt"){
+
+#all linear and select quadratic
+bugs.data$occDM <- model.matrix(~ bufferData$bio6 +
+                                    bufferData$bio5 +
+                                    bufferData$y +
+                                    bufferData$distCoast +
+                                    bufferData$tree_line +
+                                    bufferData$MountainBirchForest +
+                                    bufferData$Bog +
+                                    bufferData$ODF +
+                                    bufferData$Meadows +
+                                    bufferData$OSF +
+                                    bufferData$Mire +
+                                    bufferData$SnowBeds +
+                                    I(bufferData$bio6^2) +
+                                    I(bufferData$bio5^2) +
+                                    I(bufferData$y^2) +
+                                    I(bufferData$distCoast^2) +
+                                    I(bufferData$tree_line^2) +
+                                    I(bufferData$MountainBirchForest^2) +
+                                    I(bufferData$Bog^2) +
+                                    I(bufferData$ODF^2) +
+                                    I(bufferData$Meadows^2) +
+                                    I(bufferData$OSF^2) +
+                                    I(bufferData$Mire^2) +
+                                    I(bufferData$SnowBeds^2))[,-1]
+  
+# #predictions to full grid
+bugs.data$predDM <- model.matrix(~ siteInfo_ArtsDaten$bio6 +
+                                     siteInfo_ArtsDaten$bio5 +
+                                     siteInfo_ArtsDaten$y +
+                                     siteInfo_ArtsDaten$distCoast +
+                                     siteInfo_ArtsDaten$tree_line +
+                                     siteInfo_ArtsDaten$MountainBirchForest +
+                                     siteInfo_ArtsDaten$Bog +
+                                     siteInfo_ArtsDaten$ODF +
+                                     siteInfo_ArtsDaten$Meadows +
+                                     siteInfo_ArtsDaten$OSF +
+                                     siteInfo_ArtsDaten$Mire +
+                                     siteInfo_ArtsDaten$SnowBeds +
+                                     I(siteInfo_ArtsDaten$bio6^2) +
+                                     I(siteInfo_ArtsDaten$bio5^2) +
+                                     I(siteInfo_ArtsDaten$y^2) +
+                                     I(siteInfo_ArtsDaten$distCoast^2) +
+                                     I(siteInfo_ArtsDaten$tree_line^2) +
+                                     I(siteInfo_ArtsDaten$MountainBirchForest^2) +
+                                     I(siteInfo_ArtsDaten$Bog^2) +
+                                     I(siteInfo_ArtsDaten$ODF^2) +
+                                     I(siteInfo_ArtsDaten$Meadows^2) +
+                                     I(siteInfo_ArtsDaten$OSF^2) +
+                                     I(siteInfo_ArtsDaten$Mire^2) +
+                                     I(siteInfo_ArtsDaten$SnowBeds^2))[,-1]
+}
+
+myvars <- c("bio6","bio5","y","distCoast","tree_line","MountainBirchForest",
+            "Bog","ODF","Meadows","OSF","Mire","SnowBeds",
+            "bio6_2","bio5_2","y_2","distCoast_2","tree_line_2","MountainBirchForest_2",
+            "Bog_2","ODF_2","Meadows_2","OSF_2","Mire_2","SnowBeds_2")
+bugs.data$n.covs <- ncol(bugs.data$occDM)
 bugs.data$n.preds <- dim(bugs.data$predDM)[1]
- 
+
+myvars <- c("y",'bio6',"bio6_2","distCoast","distCoast_2",
+            "bio5","bio5_2","tree_line","tree_line_2","OSF","SnowBeds")
 
 ### fit model #################################################
 
 library(rjags)
 library(jagsUI)
 
-#params <- c("int.d","line.d.sd","year.d.sd",
-#            "beta","bpv","totalPop","Density","Dens_lt")
+params <- c("int.d","beta","meanDensity","meanExpNu","fit","fit.new",
+            "Density","g","expNuIndivs","exp","NuIndivs.new")
 
-params <- c("int.d","beta","meanDensity","meanExpNu","bpv","expNuIndivs","Density")
+#choose model - already done above now
+#modelfile <- paste(myfolder,"linetransectModel_variables.txt",sep="/")
+#modelfile <- paste(myfolder,"linetransectModel_variables_LASSO.txt",sep="/")
+#modelfile <- paste(myfolder,"linetransectModel_variables_ModelSelection.txt",sep="/")
+modelfile <- paste(myfolder,mymodel,sep="/")
 
-modelfile <- paste(myfolder,"linetransectModel_variables.txt",sep="/")
+#n.cores = as.integer(Sys.getenv("NSLOTS", "1")) 
+n.cores = as.integer(Sys.getenv("SLURM_CPUS_PER_TASK", "1")) 
+#n.cores = 3
 
 out1 <- jags(bugs.data, 
              inits=NULL, 
              params, 
              modelfile, 
              n.thin=10,
-             n.chains=3, 
-             n.burnin=20000,
-             n.iter=40000,
+             n.chains=n.cores, 
+             n.burnin=10000,
+             n.iter=20000,
              parallel=T)
 
-saveRDS(out1$summary,file="outSummary_linetransectModel_variables.rds")
+saveRDS(out1,file=paste0("out_linetransectModel_variables_",task.id,".rds"))
+saveRDS(out1$summary,file=paste0("outSummary_linetransectModel_variables_",task.id,".rds"))
 
 ### end #######################################################
 
-#v 1
-# bugs.data$occDM <- model.matrix(~ bufferData$tree_line +
-#                                   bufferData$bio1 +
-#                                   bufferData$bio5 +
-#                                   bufferData$elevation +
-#                                   bufferData$Bog +
-#                                   bufferData$Forest)[,-1]
-
-
-
-#v2
-# bugs.data$occDM <- model.matrix(~ bufferData$tree_line +
-#                                   bufferData$bio1 +
-#                                   bufferData$bio1^2 +
-#                                   bufferData$bio6 +
-#                                   bufferData$Meadows +
-#                                   bufferData$OSF +
-#                                   bufferData$ODF +
-#                                   bufferData$elevation +
-#                                   bufferData$Bog +
-#                                   bufferData$Forest)[,-1]
-
-#v3
-#as v2 except with random adm effect
-
-#v4
-#as V3 except with ESW model
-
-#5
-#as V3 except with random adm effect on the ESW model and state model
-
-#6 
-# bugs.data$occDM <- model.matrix(~ bufferData$y +
-#                                   bufferData$y^2 +
-#                                   bufferData$bio6 +
-#                                   bufferData$distCoast +
-#                                   bufferData$bio5 +
-#                                   bufferData$tree_line +
-#                                   bufferData$elevation +
-#                                   bufferData$Meadows +
-#                                   bufferData$Bog +
-#                                   bufferData$Forest)[,-1]
